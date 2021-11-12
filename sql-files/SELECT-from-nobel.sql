d71b2fc19294d3990c3f39b019b93eb6ce0f3ee3
Ben Schedin <49740457+BenSchedin@users.noreply.github.com>
2021-11-12 14:04:00 -0800

    added and started select from nobel

---
+++ sql-files/SELECT-from-nobel.sql
@@ -0,0 +1,85 @@
+-- https://sqlzoo.net/wiki/SELECT_from_Nobel_Tutorial
+
+/*
+    1. 
+
+    Change the query shown so that it displays Nobel prizes for 1950.
+*/
+
+/*
+    2. 
+
+    Show who won the 1962 prize for Literature.
+*/
+
+/*
+    3. 
+
+    Read the notes about this table. Observe the result of running this SQL command to show the name, continent and population of all countries.
+*/
+
+/*
+    4. 
+
+    Read the notes about this table. Observe the result of running this SQL command to show the name, continent and population of all countries.
+*/
+
+/*
+    5. 
+
+    Read the notes about this table. Observe the result of running this SQL command to show the name, continent and population of all countries.
+*/
+
+/*
+    1. 
+
+    Read the notes about this table. Observe the result of running this SQL command to show the name, continent and population of all countries.
+*/
+
+/*
+    1. 
+
+    Read the notes about this table. Observe the result of running this SQL command to show the name, continent and population of all countries.
+*/
+
+/*
+    1. 
+
+    Read the notes about this table. Observe the result of running this SQL command to show the name, continent and population of all countries.
+*/
+
+/*
+    1. 
+
+    Read the notes about this table. Observe the result of running this SQL command to show the name, continent and population of all countries.
+*/
+
+/*
+    1. 
+
+    Read the notes about this table. Observe the result of running this SQL command to show the name, continent and population of all countries.
+*/
+
+/*
+    1. 
+
+    Read the notes about this table. Observe the result of running this SQL command to show the name, continent and population of all countries.
+*/
+
+/*
+    1. 
+
+    Read the notes about this table. Observe the result of running this SQL command to show the name, continent and population of all countries.
+*/
+
+/*
+    1. 
+
+    Read the notes about this table. Observe the result of running this SQL command to show the name, continent and population of all countries.
+*/
+
+/*
+    1. 
+
+    Read the notes about this table. Observe the result of running this SQL command to show the name, continent and population of all countries.
+*/
