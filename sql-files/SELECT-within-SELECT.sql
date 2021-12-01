620c6375db1c595e73d3ba40b1ac34af1b211b0d
Ben Schedin <49740457+BenSchedin@users.noreply.github.com>
2021-12-01 13:44:58 -0800

    finished select within select

--- sql-files/SELECT-within-SELECT.sql
+++ sql-files/SELECT-within-SELECT.sql
@@ -65,27 +65,51 @@ WHERE continent = 'Europe'
 
     Which countries have a GDP greater than every country in Europe? [Give the name only.] (Some countries may have NULL gdp values)
 */
+SELECT name
+FROM world
+WHERE gdp > ALL (SELECT gdp
+                 FROM world
+                 WHERE (continent = 'Europe') AND (GDP > 0))
 
 /*
     7. 
 
     Find the largest country (by area) in each continent, show the continent, the name and the area:
 */
+SELECT continent, name, area
+FROM world AS x
+WHERE area >= ALL (SELECT area
+                   FROM world AS y
+                   WHERE (x.continent = y.continent) AND (area > 0))
 
 /*
     8. 
 
     List each continent and the name of the country that comes first alphabetically.
 */
-
+SELECT continent, name
+FROM world AS x
+WHERE name <= ALL (SELECT name
+                   FROM world AS y
+                   WHERE x.continent = y.continent)
 /*
     9. 
 
     Find the continents where all countries have a population <= 25000000. Then find the names of the countries associated with these continents. Show name, continent and population.
 */
+SELECT name, continent, population
+FROM world AS x
+WHERE 25000000 > ALL (SELECT population
+                      FROM world AS y
+                      WHERE x.continent = y.continent)
 
 /*
     10. 
 
     Some countries have populations more than three times that of any of their neighbours (in the same continent). Give the countries and continents.
 */
+SELECT name, continent
+FROM world AS x
+WHERE population >= ALL (SELECT population*3
+                         FROM world AS y
+                         WHERE (x.continent = y.continent) AND (x.name != y.name))
